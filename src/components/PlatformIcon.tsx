30cf347a74d8a2752ee4bf8efb93dffff6dc89bb
farion1231 <farion1231@gmail.com>
2025-01-15 16:27:56 +0800

    remove duplicate interfaces

--- src/components/PlatformIcon.tsx
+++ src/components/PlatformIcon.tsx
@@ -1,16 +1,16 @@
-import { Platform } from "../hooks/useGames";
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { HStack, Icon } from "@chakra-ui/react";
+import { BsGlobe } from "react-icons/bs";
 import {
-  FaWindows,
-  FaPlaystation,
-  FaXbox,
+  FaAndroid,
   FaApple,
   FaLinux,
-  FaAndroid,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
 } from "react-icons/fa";
-import { SiNintendo } from "react-icons/si";
 import { MdPhoneIphone } from "react-icons/md";
-import { BsGlobe } from "react-icons/bs";
+import { SiNintendo } from "react-icons/si";
+import { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   platform: Platform[];
