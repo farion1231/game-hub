cb2c31fae4b9f50515db83345cd455a56a47eece
farion1231 <farion1231@gmail.com>
2025-01-23 23:19:49 +0800

    build game trailor

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,25 @@
+import { Spinner } from "@chakra-ui/react";
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+  if (isLoading) return <Spinner />;
+  if (error) throw error;
+
+  const first = data?.results[0];
+  if (!first) return null;
+  return (
+    <video
+      src={first?.data[480]}
+      poster={first?.preview}
+      controls
+      style={{ width: "100%", height: "100%" }}
+    />
+  );
+};
+
+export default GameTrailer;
