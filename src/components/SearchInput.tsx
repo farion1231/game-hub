8998e1c03a8142649cef494b808dbf2369db183e
farion1231 <farion1231@gmail.com>
2025-01-24 00:10:50 +0800

    fix nav bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,14 +2,19 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 import { useRef } from "react";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const searchRef = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText); // 使用selector防止重新渲染
+  const navigate = useNavigate();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (searchRef.current) setSearchText(searchRef.current.value);
+    if (searchRef.current) {
+      setSearchText(searchRef.current.value);
+      navigate(`/`); //导航到主页
+    }
   };
 
   return (
